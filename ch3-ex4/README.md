19842640ddf9a8194cd698660d0abe114103d8d7
Eric W. Li <eric@ericwli.com>
2023-03-23 20:31:26 -0400

    Complete chapter 3, exercise 4

---
+++ ch3-ex4/README.md
@@ -0,0 +1,8 @@
+# Chapter 3: Exercise 4
+
+## 3.2.1 Using @Autowired to inject the values through the class fields
+
+- Use the stereotype annotation @Component to instruct Spring to create and add
+a bean of the class to the context.
+- Then, use the @Autowired annotation over the field that we want Spring
+to provide the value for from its context.
---
+++ ch3-ex4/src/main/java/com/ericwli/Person.java
@@ -0,0 +1,33 @@
+package com.ericwli;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name = "Ella";
+
+    @Autowired
+    private Parrot parrot;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+
+    public void setParrot(Parrot parrot) {
+        this.parrot = parrot;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Person name=%s]", name);
+    }
+}
